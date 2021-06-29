7443e09d23c52371d458d3be61385f3224b9822a
Exec <86654007+Exec7@users.noreply.github.com>
2021-06-29 12:14:58 +0300

    Create config.lua

---
+++ lua/autorun/server/config.lua
@@ -0,0 +1,6 @@
+autodonate = autodonate or {}
+
+autodonate["chatcommand"]={"/donate","/autodonate","!donate","!autodonate"}--say /donate
+
+autodonate["qiwi_currency"]=643--currency code(643-RUB,840-USD)
+autodonate["qiwi_token"]="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"--https://qiwi.com/api
